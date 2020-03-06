792b09fc4972f7cb3385606d3639628662a6bcfc
Jaybur1 <jayburbyga@gmail.com>
2020-03-06 17:55:17 +0000

    added indexJs to pass test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+function sum(fromN, toN) { 
+if (toN === fromN) return toN;
+return toN + sum(fromN, toN - 1);
 }
 
+console.log(sum(3,7));
 module.exports = sum;
